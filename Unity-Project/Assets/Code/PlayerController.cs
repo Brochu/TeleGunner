ec8f01e3414a414b4d0144918d2dc6c54281b26b
Brochu <alexbrochu1@gmail.com>
2014-11-22 03:02:25 -0500

    We don't need to affect rotation to the camera object, we need to affect it to the player object to be able to keep the forward vector to a valid value

--- Unity-Project/Assets/Code/PlayerController.cs
+++ Unity-Project/Assets/Code/PlayerController.cs
@@ -3,30 +3,20 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    public Camera playerCam = null;
     // Constant used to check if input axis close to 0
-    private bool updating = true;
     private const float EPSILON = 0.0001f;
 
     // Use this for initialization
     void Start () {
         // Get useful components
-        if (playerCam == null)
-        {
-            updating = false;
-            Debug.LogWarning("---- Player cam property for object " + name + " is not set ! ----");
-        }
     }
     
     // Update is called once per frame
     void Update () {
 
-        if (!updating)
-            return;
-
         // TODO: Get player presses and handle behaviour
         transform.position += getPlayerDesiredMoveDirection();
-        playerCam.transform.rotation = getPlayerDesiredLookDirection();
+        transform.rotation = getPlayerDesiredLookDirection();
     }
 
     private Vector3 getPlayerDesiredMoveDirection()
