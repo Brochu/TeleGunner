540212fc8035affba9cdaca4270b823e66ad902b
Brochu <alexbrochu1@gmail.com>
2014-11-25 05:21:26 -0500

    Setup a simple way to make some hit effect appear when hitting a wall, TODO: move the hit effect to the gun data

--- Unity-Project/Assets/Code/Gun.cs
+++ Unity-Project/Assets/Code/Gun.cs
@@ -3,12 +3,28 @@ using System.Collections;
 
 public class Gun : MonoBehaviour {
 
-    public SOGunData data;
+    public SOGunData data = null;
+    public GameObject tempHitEffect = null;
 
     public void shoot(Vector3 direction)
     {
         // Add shoot logic
         Debug.Log("POW !");
         Debug.DrawRay(gameObject.transform.position, direction * 500, Color.red);
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(gameObject.transform.position, direction * 500, out hitInfo, 500))
+        {
+            Object o = Instantiate(tempHitEffect, hitInfo.point, Quaternion.LookRotation(hitInfo.transform.forward));
+            StartCoroutine(killHitEffect(o));
+        }
+    }
+
+    IEnumerator killHitEffect(Object o)
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        Debug.Log("Destroy hit effect");
+        Object.Destroy(o);
     }
 }
