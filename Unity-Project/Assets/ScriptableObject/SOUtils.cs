1745071776ec7163bdc8e2191c0e191fd5e576ab
Brochu <alexbrochu1@gmail.com>
2014-11-25 04:22:54 -0500

    Made the script to create scriptable object work in a more simplified way and created first gun data object

--- Unity-Project/Assets/ScriptableObject/SOUtils.cs
+++ Unity-Project/Assets/ScriptableObject/SOUtils.cs
@@ -1,46 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SOUtils {
+public class SOGunData : ScriptableObject
+{
+    public string gunName = "Gun name here";
+    public float damage = 1.0f;
+    public float rateOfFire = 1.0f;
+
 #if UNITY_EDITOR
-    public static void CreateAsset(System.Type aType, string aBaseName)
+    const string editorMenuName = "GunData";
+    [UnityEditor.MenuItem("GameObject/Create Scriptable Object/Create " + editorMenuName, false, 0),
+        UnityEditor.MenuItem("Assets/Create/Create Scriptable Object/" + editorMenuName, false, 101)]
+    public static void CreateAsset()
     {
-        ScriptableObject style = ScriptableObject.CreateInstance(aType);
+        ScriptableObject s = ScriptableObject.CreateInstance(typeof(SOGunData));
 
         string path = UnityEditor.AssetDatabase.GetAssetPath(UnityEditor.Selection.activeInstanceID);
         if (System.IO.Path.GetExtension(path) != "") path = System.IO.Path.GetDirectoryName(path);
         if (path == "") path = "Assets";
 
-        string name = path + "/" + aBaseName + ".asset";
+        string name = path + "/" + editorMenuName + ".asset";
         int id = 0;
-        while (UnityEditor.AssetDatabase.LoadAssetAtPath(name, aType) != null)
+        while (UnityEditor.AssetDatabase.LoadAssetAtPath(name, typeof(SOGunData)) != null)
         {
             id += 1;
-            name = path + "/" + aBaseName + id + ".asset";
+            name = path + "/" + editorMenuName + id + ".asset";
         }
 
-        UnityEditor.AssetDatabase.CreateAsset(style, name);
+        UnityEditor.AssetDatabase.CreateAsset(s, name);
         UnityEditor.AssetDatabase.SaveAssets();
 
         UnityEditor.EditorUtility.FocusProjectWindow();
-        UnityEditor.Selection.activeObject = style;
+        UnityEditor.Selection.activeObject = s;
     }
 #endif
-
-    public class SOGunData : ScriptableObject
-    {
-        public string gunName = "Gun name here";
-        public float damage = 1.0f;
-        public float rateOfFire = 1.0f;
-
-#if UNITY_EDITOR
-        const string editorMenuName = "GunData";
-        [UnityEditor.MenuItem("GameObject/Create Scriptable Object/Create " + editorMenuName, false, 0),
-         UnityEditor.MenuItem("Assets/Create/Create Scriptable Object/" + editorMenuName, false, 101)]
-        public static void CreateAsset()
-        {
-            SOUtils.CreateAsset(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType, editorMenuName);
-        }
-#endif
-    }
 }
